86d15043482e375fca43d4862593fb5cf95c93ca
zhongfuchun <zfcjun@163.om>
2016-12-19 19:45:37 -0800

    number

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-ok is ok  
-test is ok
-
-----------------------
-consul/etcd/zookeeper
---------------------
-/link
--------------------
-/mysql/redis/nginx/
-------------------
-/test is ok
+number is number 
